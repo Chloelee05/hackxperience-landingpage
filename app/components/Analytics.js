9ccdbac2df26cf2fd11c576494f94361ed82de4d
Moe <moe.moepyesone77@gmail.com>
2026-04-08 14:47:02 +0800

    add Google Analytics

---
+++ app/components/Analytics.js
@@ -0,0 +1,18 @@
+'use client'
+
+import { useEffect } from 'react'
+import { usePathname } from 'next/navigation'
+
+export default function Analytics() {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.gtag) {
+      window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+        page_path: pathname,
+      })
+    }
+  }, [pathname])
+
+  return null
+}
